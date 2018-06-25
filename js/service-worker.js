5dc853ca76ce776aaa2d45c69672e9d48a2cc830
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-06-25 21:26:43 +0200

    descriptions

--- js/service-worker.js
+++ js/service-worker.js
@@ -35,13 +35,13 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
-});
+});  //activation
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
-    })
+    })  //fetch
     .catch(err => console.log(err, event.request))
   );
 });
